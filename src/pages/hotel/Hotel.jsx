cdb1f1e542fe600222697fe5e6b0151911de4520
Aryan Sethi <aryan.rdps@gmail.com>
2022-05-15 23:18:11 +0530

    Added image slider

--- src/pages/hotel/Hotel.jsx
+++ src/pages/hotel/Hotel.jsx
@@ -5,9 +5,18 @@ import Header from "../../components/header/Header";
 import Footer from "../../components/footer/Footer";
 import MailList from "../../components/mailList/MailList";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faLocationDot } from "@fortawesome/free-solid-svg-icons";
+import {
+    faCircleArrowLeft,
+    faCircleArrowRight,
+    faCircleXmark,
+    faLocationDot
+} from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
 
 const Hotel = () => {
+    const [slideNumber, setSlideNumber] = useState(0);
+    const [open, setOpen] = useState(false);
+
     const photos = [
         {
             src: "https://cf.bstatic.com/xdata/images/hotel/max1280x900/261707778.jpg?k=56ba0babbcbbfeb3d3e911728831dcbc390ed2cb16c51d88159f82bf751d04c6&o=&hp=1"
@@ -34,8 +43,42 @@ const Hotel = () => {
             <Navbar />
             <Header type="list" />
             <div className="hotelContainer">
+                {open && (
+                    <div className="slider">
+                        <FontAwesomeIcon
+                            icon={faCircleXmark}
+                            className="close"
+                            onClick={() => setOpen(prev => false)}
+                        />
+                        <FontAwesomeIcon
+                            icon={faCircleArrowLeft}
+                            className="arrow"
+                            onClick={() =>
+                                setSlideNumber(prev =>
+                                    prev === 0 ? photos.length - 1 : prev - 1
+                                )
+                            }
+                        />
+                        <div className="sliderWrapper">
+                            <img
+                                className="sliderImg"
+                                src={photos[slideNumber].src}
+                                alt=""
+                            />
+                        </div>
+                        <FontAwesomeIcon
+                            icon={faCircleArrowRight}
+                            className="arrow"
+                            onClick={() =>
+                                setSlideNumber(prev =>
+                                    prev === photos.length - 1 ? 0 : prev + 1
+                                )
+                            }
+                        />
+                    </div>
+                )}
                 <div className="hotelWrapper">
-					<button className="bookNow">Reserve or Book Now!</button>
+                    <button className="bookNow">Reserve or Book Now!</button>
                     <h1 className="hotelTitle">Grand Hotel</h1>
                     <div className="hotelAddress">
                         <FontAwesomeIcon icon={faLocationDot} />
@@ -49,12 +92,16 @@ const Hotel = () => {
                         airport taxi
                     </span>
                     <div className="hotelImages">
-                        {photos.map(photo => (
-                            <div className="hotelImgWrapper">
+                        {photos.map((photo, index) => (
+                            <div className="hotelImgWrapper" key={index}>
                                 <img
                                     className="hotelImg"
                                     src={photo.src}
                                     alt=""
+                                    onClick={() => {
+                                        setSlideNumber(prev => index);
+                                        setOpen(prev => true);
+                                    }}
                                 />
                             </div>
                         ))}
@@ -95,8 +142,8 @@ const Hotel = () => {
                         </div>
                     </div>
                 </div>
-				<MailList />
-				<Footer />
+                <MailList />
+                <Footer />
             </div>
         </div>
     );
