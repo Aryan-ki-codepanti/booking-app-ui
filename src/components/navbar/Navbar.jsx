e53e637494bd0655c53ae832b70037428149f644
Aryan Sethi <aryan.rdps@gmail.com>
2022-05-19 18:04:30 +0530

    homepage ready

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,17 +1,23 @@
-import './navbar.css';
+import { Link } from "react-router-dom";
+import "./navbar.css";
 
 const Navbar = () => {
-  return (
-    <div className='navbar'>
-        <div className="navContainer">
-            <span className="logo">aryanbooking</span>
-            <div className="navItems">
-                <button className="navButton">Register</button>
-                <button className="navButton">Login</button>
+    return (
+        <div className="navbar">
+            <div className="navContainer">
+                <Link
+                    to="/"
+                    style={{ color: "inherit", textDecoration: "none" }}
+                >
+                    <span className="logo">aryanbooking</span>
+                </Link>
+                <div className="navItems">
+                    <button className="navButton">Register</button>
+                    <button className="navButton">Login</button>
+                </div>
             </div>
         </div>
-    </div>
-  )
+    );
 };
 
 export default Navbar;
